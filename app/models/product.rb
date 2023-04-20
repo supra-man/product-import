96fdbd79876c375be2dbab32219dbf0069718dbf
Supradipta <suprashah@gmail.com>
2023-04-20 21:07:00 +0545

    Added action storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                                                              :bigint           not null, primary key
+#  #<ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition :string
+#  code                                                            :string           not null
+#  name                                                            :string
+#  created_at                                                      :datetime         not null
+#  updated_at                                                      :datetime         not null
+#
 class Product < ApplicationRecord
+  has_many_attached :images, :dependent => :destroy
 end
